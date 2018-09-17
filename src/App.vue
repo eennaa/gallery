f9c7586e3ef609e022115bd2ddaa2253bb72996d
Ena <ukradenitrenutak@gmail.com>
2018-09-17 21:02:05 +0200

    Navbar (first version)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    
+    <layout/>
   </div>
 </template>
 
 <script>
-
+import Layout from './components/Layout.vue'
 
 export default {
   name: 'app',
   components: {
-    
+    Layout
   }
 }
 </script>
